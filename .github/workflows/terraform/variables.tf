cee9c665c189e27f5ba7409232b6c61cf05ced2e
oaadonsgithub <77804816+oaadonsgithub@users.noreply.github.com>
2025-05-04 14:00:26 -0700

    Initial commit for CodeDeploy automation_workflow_6

--- .github/workflows/terraform/variables.tf
+++ .github/workflows/terraform/variables.tf
@@ -1,10 +1,37 @@
-variable "aws_region" {
-  description = "The AWS region to deploy to"
+variable "vpc_id" {}
+variable "subnet_ids" {
+  type = list(string)
+}
+variable "ami_id" {}
+variable "instance_type" {
+  default = "t2.medium"
+}
+variable "key_name" {}
+variable "lb_target_group_443_name" {
+  description = "Name prefix for the 443 target group"
   type        = string
-  default     = "us-west-1"
+}
+variable "aws_account_id" {
+  description = "AWS account ID"
+  type        = string
+}
+variable "aws_account_region" {
+  description = "AWS region"
+  type        = string
+}
+variable "region" {
+  description = "AWS region"
+  type        = string
 }
 
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
+variable "active_color" {
+  description = "The active deployment color: 'blue' or 'green'"
+  type        = string
+  default     = "blue"
+}
+
+
+variable "certificate_arn" {
+  description = "The ARN of the SSL/TLS certificate for the HTTPS listener"
   type        = string
 }
